96d90d467bbb208f36bc65118082fd14f3182a06
Snyvaa <ceyonnnnn@gmail.com>
2024-08-31 21:05:59 +0700

    Update LinkedListNew.py

--- LinkedListNew.py
+++ LinkedListNew.py
@@ -25,134 +25,102 @@ class LinkedList:
             temp_node = temp_node.next
         return result
     
-    def append(self, value):
+    def append(self,value):
         new_node = Node(value)
-        if self.head is None:
+        if self.head == None:
             self.head = new_node
             self.tail = new_node
         else:
             self.tail.next = new_node
             self.tail = new_node
         self.length += 1
-    
-    def prepend(self, value):
+
+    def prepend(self,value):
         new_node = Node(value)
-        if self.head is None:
+        if self.head == None:
             self.head = new_node
             self.tail = new_node
         else:
             new_node.next = self.head
             self.head = new_node
-        self.length += 1
-    
-    def insert(self, index, value):
+        self.length +=1
+
+    def insert(self,index,value):
         new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-        elif index == 0:
-            new_node.next = self.head
-            self.head = new_node
-        else:
-            temp_node = self.head
-            for _ in range(index-1):
-                temp_node = temp_node.next
-            new_node.next = temp_node.next
-            temp_node.next = new_node
-        self.length += 1
-    
-    def traverse(self):
-        current = self.head
-        while current is not None:
-            print(current.value)
-            current = current.next
-    
-    def search(self, target):
-        current = self.head
-        while current is not None:
-            if current.value == target:
-                return True
-            current = current.next
-        return False
-    
-    def search(self, target):
-        current = self.head
-        index = 0
-        while current is not None:
-            if current.value == target:
-                return index
-            current = current.next
-            index += 1
-        return -1
-    
-    def get(self, index):
-        if index == -1:
-            return self.tail
-        elif index < -1 or index >= self.length:
+        if index < 0 or index >= self.length:
             return None
-        current = self.head
-        for _ in range(index):
-            current = current.next
-        return current
+        temp = self.head
+        for i in range(index-1):
+            temp = temp.next
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+
+    def traversal(self):
+        tem = self.head
+        for i in range(self.length):
+            tem = tem.next
     
-    def set_value(self, index, value):
-        temp = self.get(index)
-        if temp:
-            temp.value = value
-            return True
-        return False
+    def search(self,value):
+        tem = self.head
+        for i in range(self.length):
+            if tem.value == value:
+                return print('Yes')
+            tem = tem.next
+        return print('No')
+    
+    def get(self,index):
+        tem = self.head
+        if self.length > index:
+            for i in range(index):
+                tem = tem.next
+            print(tem.value)
+            return tem
+        else:
+            return False
+    
+    def set_value(self,index, value):
+        tem = self.get(index)
+        tem.value = value
     
     def pop_first(self):
-        if self.length == 0:
-            return None
         popped_node = self.head
-        if self.length == 1:
-            self.head = None
-            self.tail = None
-        else:
-            self.head = self.head.next
-            popped_node.next = None
-        self.length -= 1
-        return popped_node
-    
-    
+        self.head = self.head.next
+        popped_node.next = None
     
     def pop(self):
-        if self.length == 0:
-            return None
-        popped_node = self.tail
-        if self.length == 1:
-            self.head = self.tail = None
-        else:
-            temp = self.head
-            while temp.next is not self.tail:
-                temp = temp.next
-            temp.next = None
-            self.tail = temp
-        self.length -= 1
-        return popped_node
-    
+        pop = self.head
+        while pop.next is not self.tail:
+            pop = pop.next
+        pop.next = None
+        self.tail = pop
 
-    def remove(self, index):
-        if index < -1 or index >= self.length:
-            return None
-        if index == 0:
-            return self.pop_first()
-        if index == -1 or index == self.length-1:
-            return self.pop()
-        prev_node = self.get(index-1)
-        popped_node = prev_node.next
-        prev_node.next = popped_node.next
-        popped_node.next = None
-        self.length -= 1
-        return popped_node
-    
+    def remove(self,index):
+        pre = self.get(index-1)
+        remove = pre.next
+        pre.next = remove.next
+        remove.next = None
+
+
+
+
 
 linked_list = LinkedList()
-print(linked_list)
 linked_list.append(10)
-linked_list.append(20)
+linked_list.prepend(20)
 linked_list.append(30)
-linked_list.append(40)
+linked_list.insert(2,40)
+linked_list.traversal()
+linked_list.get(2)
+linked_list.search(30)
+linked_list.insert(1,50)
 print(linked_list)
+linked_list.set_value(1,60)
 print(linked_list)
+linked_list.pop_first()
+print(linked_list)
+linked_list.pop()
+print(linked_list)
+linked_list.remove(2)
+print(linked_list)
+
